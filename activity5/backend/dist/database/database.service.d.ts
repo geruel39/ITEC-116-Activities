16fc77fb323a3167adc9e1c3394b3a4747badd39
Geruel <geruelalcaraz@gmail.com>
2025-10-08 10:48:08 +0800

    cleanup

--- activity5/backend/dist/database/database.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { OnModuleDestroy, OnModuleInit } from "@nestjs/common";
-export declare class DatabaseService implements OnModuleInit, OnModuleDestroy {
-    private pool;
-    onModuleInit(): Promise<void>;
-    query(sql: string, params?: any[]): Promise<import("mysql2/promise").QueryResult>;
-    onModuleDestroy(): Promise<void>;
-}
